ee16e9115c788cc554155de4989b87ec39dff294
Faisal-pixel <adamsfaisal2003@gmail.com>
2024-12-27 01:59:44 +0100

    fixed log out issue: now all watchlist clears when a user logs out

--- src/app/components/AccountDropdown.tsx
+++ src/app/components/AccountDropdown.tsx
@@ -4,46 +4,50 @@ import DropdownMenuComponent from "./shared/DropdownMenuComponent";
 import DotDotDot from "../../assets/icons/dot dot dot.svg";
 import { AuthContext } from "../context/AuthContext";
 import { useToast } from "@/hooks/use-toast";
+import { WatchListsContext } from "../context/WatchListsContext";
 
 // type Props = {}
-
 
 const menuItemClasses = "inline-block w-full";
 
 const AccountDropdown = () => {
-    const {toast} = useToast();
-    const {isAuthenticated, logOut} = useContext(AuthContext);
+  const { toast } = useToast();
+  const { isAuthenticated, logOut } = useContext(AuthContext);
+  const { setWatchlists } = useContext(WatchListsContext);
 
-    const onClickLogout = () => {
-        logOut();
-        toast({
-            title: "Logged out successfully"
-        });
-    }
+  const onClickLogout = () => {
+    setWatchlists([]);
+    logOut();
+    toast({
+      title: "Logged out successfully",
+    });
+  };
 
-    const menuItems = isAuthenticated ? [
+  const menuItems = isAuthenticated
+    ? [
         {
-            label: "Logout",
-            className: menuItemClasses,
-            onClick: onClickLogout
+          label: "Logout",
+          className: menuItemClasses,
+          onClick: onClickLogout,
         },
         {
-            label: "Profile",
-            className: menuItemClasses,
-            href: "/profile"
-        }
-    ] : [
+          label: "Profile",
+          className: menuItemClasses,
+          href: "/profile",
+        },
+      ]
+    : [
         {
-            label: "Login",
-            href: "/login",
-            className: menuItemClasses
+          label: "Login",
+          href: "/login",
+          className: menuItemClasses,
         },
         {
-            label: "Sign up",
-            href: "/signup",
-            className: menuItemClasses
-        }
-    ];
+          label: "Sign up",
+          href: "/signup",
+          className: menuItemClasses,
+        },
+      ];
   return (
     <DropdownMenuComponent menuLabel="My Account" menuItems={menuItems}>
       <div className="flex justify-center items-center">
